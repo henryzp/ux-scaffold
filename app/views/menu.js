0388df61e174721a6139dfba703878deca1ce03e
Der <der0846@gmail.com>
2013-04-17 17:08:48 +0800

    add sidenav

--- app/views/menu.js
+++ app/views/menu.js
@@ -1,18 +1,27 @@
-KISSY.add("app/views/menu",function(S,View){
+KISSY.add("app/views/menu", function(S, View, Sidenav) {
     return View.extend({
-        init:function(){
+        init: function() {
             this.observeLocation({
-                pathname:true
+                pathname: true
             });
         },
-        render:function(){
+        render: function() {
             var me = this;
             me.setViewPagelet({
                 //动态数据
-            }, function() {
+            }, function() {});
+
+             new Sidenav({
+                el: '#sidebar',
+                index: '#!/promotion/index/',
+                duration: 0.25,
+                pathMap: {
+                    '#!/promotion/campaign/': '#!/promotion/index/',
+                    '#!/promotion/taokeaudit_history/': '#!/promotion/index/'
+                }
             });
         }
     });
-},{
-    requires:['mxext/view']
+}, {
+    requires: ['mxext/view', 'brix/gallery/sidenav/index', 'brix/gallery/sidenav/index.css']
 });
